dd0bf4b365001d8cf76327e0082d696b3eb0da5b
letty23244 <akulluleticia23@gmail.com>
2024-10-10 10:50:39 +0300

    work

---
+++ question.py
@@ -0,0 +1,5 @@
+# create a  python program that calculates the area of a triangle
+base= input("enter the base")
+height = input("enter the height")
+area = (1/2 * base * height)
+print =(area)
---
+++ question_two.py
@@ -0,0 +1,8 @@
+# using a function create a program that calculates the volume of a cylinder
+radius = int(input("enter the radius " ))
+height = int(input("enter the height"))
+import math
+pie_value =3.14
+pie_value = math.pi
+volume_of_acylinder = pie_value *(radius**2) * height
+print(volume_of_acylinder)
